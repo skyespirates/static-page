80d627b102b581eabbd644d8f0f2d1ae78a34fe0
Skyes Crawford <akashiseijurou414@gmail.com>
2021-09-24 18:00:41 +0700

    clear projects

--- navbar-3/app.js
+++
@@ -1,6 +0,0 @@
-const burger = document.querySelector(".burger");
-const menus = document.querySelector(".menus");
-
-burger.addEventListener("click", () => {
-  menus.classList.toggle("active");
-});
--- navbar/scripts.js
+++
@@ -1,6 +0,0 @@
-const bar = document.querySelector(".navbar__bar");
-const menu = document.querySelector(".navbar__menu");
-
-bar.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
